ac299221ef1a2641ba6d634d5e45f17addd0c13c
vamahaja <vamahaja@redhat.com>
2019-05-07 16:04:31 +0530

    Upadate AMQ load script as per new changes

--- ansible-deploy-workload/roles/deploy-amq-workload/files/start-amq-load.sh
+++ ansible-deploy-workload/roles/deploy-amq-workload/files/start-amq-load.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 ### This script is used for run workload on amq pod
-### eg. sh run-workload-amq.sh
-
-
 ### variables
 nameSpace=$1
 podName=$2
@@ -18,9 +15,11 @@ N='\033[0m'
 
 echo | tee -a $outputFile
 
-for i in $(seq 1 $iterations); do
+i_index=1
+while true;
+do
     echo -e "${G}Running transactions on database${N}"
-    echo "------ Running iteration $i ------" | tee -a $outputFile
+    echo "------ Running iteration $i_index ------" | tee -a $outputFile
 
     ### producing messages
     echo "****** Producing Messages ******" | tee -a $outputFile
@@ -31,4 +30,5 @@ for i in $(seq 1 $iterations); do
     echo "****** Consuming Messages ******" | tee -a $outputFile
     (time oc -n $nameSpace exec -i $podName -- bash -c "./broker/bin/artemis consumer --message-count=$messageCount")  2>&1 |& tee -a $outputFile
     echo | tee -a $outputFile
+    i_index=`expr $i_index + 1`
 done
