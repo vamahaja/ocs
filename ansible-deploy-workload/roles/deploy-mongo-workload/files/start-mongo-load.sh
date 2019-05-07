d63be95a96890df40d28b68ec6d85dd984dff3c5
vamahaja <vamahaja@redhat.com>
2019-05-07 15:35:26 +0530

    Add support to execute infinite iterations load on MongoDb

--- ansible-deploy-workload/roles/deploy-mongo-workload/files/start-mongo-load.sh
+++ ansible-deploy-workload/roles/deploy-mongo-workload/files/start-mongo-load.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 ### This script is used for run load on mongodb pod
-### eg. sh run-workload-mongodb.sh
 
 nameSpace=$1
 mongodbPodName=$2
 ycsbPodName=$3
 mongodbIP=$4
 
-threads=10
-recordCount=1000
-operationCount=1000
-iterations=10
+threads=$5
+recordCount=$6
+operationCount=$7
 
 outputFile=output-mongodb-pod-$ycsbPodName-threads-$threads-recordCount-$recordCount-operationCount-$operationCount.$$
 
 G='\033[1;32m'
 N='\033[0m'
 
-for i in $(seq 1 $iterations); do
-    echo -e "\n------ Running iteration $i ------" | tee -a $outputFile
+i_index=1
+while true;
+do
+    echo -e "\n------ Running iteration $i_index ------" | tee -a $outputFile
     ### delete database
     echo -e "\n${G}Deleting Database${N}"
     echo -e "\n****** Deleting existing database ******" >> $outputFile
@@ -38,4 +38,5 @@ for i in $(seq 1 $iterations); do
     (time oc -n $nameSpace exec -i $ycsbPodName -- bash -c \
         "./bin/ycsb run mongodb -s -threads $threads -P "workloads/workloadf" -p mongodb.url=mongodb://redhat:redhat@$mongodbIP:27017/sampledb -p recordcount=$recordCount \
         -p operationcount=$operationCount") 2>&1 |& tee -a $outputFile
+    i_index=`expr $i_index + 1`
 done
